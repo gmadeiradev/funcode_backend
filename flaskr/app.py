a7e6c3ab504a8905597692eae840d1b995c060f0
Gabriel da Silva Madeira <63795256+gmadeiradev@users.noreply.github.com>
2021-06-14 10:33:23 -0300

    add env file and uptade soma args

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,12 +2,12 @@ from flask import Flask
 from flaskr.extensions import configuration
 
 
-def minmal_app():
+def minmal_app(**config):
     app=Flask(__name__)
-    configuration.init_app(app)
+    configuration.init_app(app, **config)
 
 
-def create_app():
-    app = minmal_app()
+def create_app(**config):
+    app = minmal_app(**config)
     configuration.load_extensions(app)
     return app
